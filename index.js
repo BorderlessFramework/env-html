00688f5add5f796d9af3733da50786ab3882b5b0
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2022-07-29 22:19:45 -0400

    Added boilerplate .html file from mockup and a fake build step

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-export default function () {
-  console.log("env-html module reporting for duty");
+import { fileURLToPath } from "node:url";
 
+export default function (config) {
   return {
-    src: "src/",
-    boilerplate: false,
+    src: "__site/",
+    boilerplate: fileURLToPath(new URL("boilerplate/", import.meta.url)),
+    buildCommand: "rm __site/index.js",
+    buildOutputFolder: "__site/", // no build necessary, just send it as is
   };
 }
