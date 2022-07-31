325e81b44f9459cc66389938e487c03f2b92763d
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2022-07-30 20:10:23 -0400

    Deploy command

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ export default function (config) {
     boilerplate: fileURLToPath(new URL("boilerplate/", import.meta.url)),
     buildCommand: "rm __site/index.js",
     buildOutputFolder: "__site/", // no build necessary, just send it as is
+    getDeployCommand: (deployment) => {
+      `echo "Deploying to ${deployment.host}"`;
+    },
   };
 }
